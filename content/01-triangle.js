d67c6a7528004c5de144e4304c37b349c4e93cfc
Matt Chang <chchwy@gmail.com>
2019-10-03 23:43:42 +1000

    01: bind buffers

--- content/01-triangle.js
+++ content/01-triangle.js
@@ -3,6 +3,24 @@
 let gl = null;
 let canvas = null;
 
+function createVertexBuffer() {
+  let vertices = [
+    -50.0, 50.0, 0.0,
+    -50.0,-50.0, 0.0,
+    50.0,-50.0, 0.0,
+    50.0, 50.0, 0.0];
+  let vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  let indices = [0, 1, 2, 1, 2, 3];
+  let indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+}
+
 function main(canvasId) {
   console.log('01!');
 
@@ -16,7 +34,7 @@ function main(canvasId) {
   if (!gl)
     console.error("Context is null!");
 
-  gl.clearColor(0.92, 0.91, 0.68, 1.0);
+  gl.clearColor(0, 0, 0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
